ec368dd72fb49ab345e1e3a485265eb718f57320
harunurhan <harunurhan17@gmail.com>
2018-09-13 15:50:31 +0200

    support config file for options

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,25 @@
 import * as program from 'commander';
+import { resolve } from 'path';
 
-import yap from './yap';
+import { requireMaybeExistentJson } from './fs';
 import { YapArgs } from './types';
+import yap from './yap';
 
 // TODO: add args and options validations based on ./types
 program
+  .option(
+    '-c, --configPath <configPath>',
+    'Path to config json file, defaults to yapconfig.json',
+    resolve('yapconfig.json'))
   .option('-d, --baseDir <baseDir>', 'Base dir for generating files, defaults to cwd', process.cwd())
-  .option('-s, --scaffoldsDir <scaffoldsDir>', 'Where scaffolds are located', '__scaffolds__')
+  .option('-s, --scaffoldsDir <scaffoldsDir>', 'Dir where scaffolds are, defaults to __scaffolds__', '__scaffolds__')
   .arguments('<scaffold> <name>')
   .action(() => {
-    const { scaffoldsDir, baseDir } = program;
-    yap(program.args as YapArgs, { scaffoldsDir, baseDir });
+    const { scaffoldsDir, baseDir, configPath } = program;
+    const options = {
+      ...requireMaybeExistentJson(configPath),
+      baseDir,
+      scaffoldsDir,
+    };
+    yap(program.args as YapArgs, options);
   }).parse(process.argv);
